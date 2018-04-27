1f8f8262babaf5c1b079dd4960c45d659bdbfabb
Pie <highsierrapie@Highs-iMac.fritz.box>
2018-04-27 14:04:23 +0200

    delegate Main and Store to App, render App, combine reducers and routing

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,41 +1,24 @@
-/*
-  Import Dependencies
-*/
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
 
-/*
-  Import Components
-*/
-import Main from './components/Main'
+import App from './components/App'
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
-/* Import CSS */
 import css from  './styles/style.styl'
 
-/* Import our data store */
-
-
-/*
-  Error Logging
-*/
-
-// import Raven from 'raven-js';
-// import { sentry_url } from './data/config';
-// import { Route } from 'react-router';
-//if(window) {
-//   Raven.config(sentry_url).install();
-// }
-
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
--- client/store.js
+++ client/store.js
@@ -2,14 +2,12 @@ import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router'
 
-
 import rootReducer from './reducers/index'
 
 import comments from './data/comments';
 import posts from './data/posts';
 /*
   Store
-
   Redux apps have a single store which takes
   1. All Reducers which we combined into `rootReducer`
   2. An optional starting state - similar to React's getInitialState
@@ -22,20 +20,6 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState);
 
-// we export history because we need it in `reduxstagram.js` to feed into <Router>
 export const history = syncHistoryWithStore(browserHistory, store);
-
-/*
-  Enable Hot Reloading for the reducers
-  We re-require() the reducers whenever any new code has been written.
-  Webpack will handle the rest
-*/
-
-if(module.hot) {
-  module.hot.accept('./reducers/', () => {
-    const nextRootReducer = require('./reducers/index').default;
-    store.replaceReducer(nextRootReducer);
-  });
-}
 
 export default store;
